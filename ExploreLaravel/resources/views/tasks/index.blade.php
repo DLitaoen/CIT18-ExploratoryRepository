a146d6ea2d41440b1400439b3a728f4a818348c3
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-02-15 21:10:24 +0800

    Added simple index view for tasks

---
+++ ExploreLaravel/resources/views/tasks/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task CRUD</title>
+</head>
+<body>
+    <h1>Task CRUD</h1>
+    <p>This page was made to show an example of CRUD operations for Laravel.</p>    
+</body>
+</html>
